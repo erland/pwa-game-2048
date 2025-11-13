78a184078ab19e91d9463eabb2b0c45745d4ad4e
Erland Lindmark <erland@isaksson.info>
2025-11-13 12:27:38 +0100

    Adjusted so game starts immediately

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -9,9 +9,8 @@ export class MenuScene extends BaseMenuScene {
     this.add.rectangle(0, 0, width, height, 0x101018).setOrigin(0, 0);
   }
 
-  /** Hook to add extra UI without overriding create(). */
   protected afterCreate(): void {
-    const { width, height } = this.scale;
-    this.add.text(width * 0.5, height * 0.75, 'This is the Base* menu', this.getTheme().typography.small).setOrigin(0.5);
+    // Start game immedately
+    this.startGame();
   }
 }
